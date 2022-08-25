4912d38876c66df7760f153814cabfe538b458b2
vmjcv666 <vmjcv666@gmail.com>
2022-08-25 16:32:33 +0800

    add icon

--- addons/fit-size/plugin.gd
+++ addons/fit-size/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("FitSize", "Control", preload("main.gd"), Texture.new())
+	add_custom_type("FitSize", "Control", preload("main.gd"), preload("icon.png"))
 
 func _exit_tree():
 	remove_custom_type("FitSize")
